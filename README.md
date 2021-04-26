b7ee676c4227e89a692447963ae13d94a286b724
mohmmad701 <mohammadriziq70@yahoo.com>
2021-04-26 21:35:07 +0300

    lab02

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# lab02
+introduction About my Project:
+
+We did this project using HTML, CSS and JavaScript to give the program a better look and better formatting using CSS We created an HTML file that includes the person’s information and a brief summary of it and we formatted the screen using javascript Then we used and created five questions and then answered either yes or no using a specific query
---
+++ javascript/app.js
@@ -0,0 +1,110 @@
+'use strict';
+let login=prompt('enter your name');
+alert('wellcome  '+login);
+
+
+let g1=prompt('do you like a red color ?');
+switch(g1.toLowerCase()){
+    case 'yes':
+        case 'y':
+   // console.log('yes i like this color');
+    alert('i like this color');
+    break;
+    case 'no':
+        case 'n':
+       // console.log('no i like deferant color');
+    alert(' no i like a deferant color');
+    break;
+    default:
+       // console.log('plese ask me agin');
+        alert('plese ask me again');
+        break;
+
+}
+
+
+
+let g2=prompt('do you have a pin ?');
+switch(g2.toLowerCase()){
+    case 'yes':
+        case 'y':
+   // console.log('yes i have one');
+    alert('yes i have one');
+    break;
+    case 'no':
+        case 'n':
+       // console.log('no i dont have');
+    alert(' no i dont have');
+    break;
+    default:
+       // console.log('plese ask me agin');
+        alert('plese ask me again');
+        break;
+
+}
+let g3=prompt('do you like a mansaf ?');
+switch(g3.toLowerCase()){
+    case 'yes':
+        case 'y':
+   // console.log('yes of course');
+    alert('yes of course');
+    break;
+    case 'no':
+        case 'n':
+       // console.log('no i dont like');
+    alert(' no i dont like');
+    break;
+    default:
+       // console.log('plese ask me agin');
+        alert('plese ask me again');
+        break;
+
+}
+let g4=prompt('do you like my car ?');
+switch(g4.toLowerCase()){
+    case 'yes':
+        case 'y':
+   // console.log('yes i like');
+    alert('yes of course');
+    break;
+    case 'no':
+        case 'n':
+       // console.log('no i dont like ');
+    alert(' no its very old');
+    break;
+    default:
+       // console.log('plese ask me agin');
+        alert('plese ask me again');
+        break;
+
+}
+let g5=prompt('do you have a questions?');
+switch(g5.toLowerCase()){
+    case 'yes':
+        case 'y':
+   // console.log('yes of course');
+    alert('yes of course');
+    break;
+    case 'no':
+        case 'n':
+       // console.log('no i dont have any question');
+    alert(' no i dont have any question');
+    break;
+    default:
+       // console.log('plese ask me agin');
+        alert('plese ask me again');
+        break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
